391ecf8e807f9ba01d4736a92407c9839746a864
Kalle <info@kalleminkner.de>
2025-03-31 12:53:28 +0200

    bump version to 0.0.3

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace MiniAdmin
 {
     public partial class MiniAdmin
     {
-        public override string ModuleVersion => "0.0.2";
+        public override string ModuleVersion => "0.0.3";
     }
 }
