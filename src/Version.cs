31edfd3ea92b28e4a13ddd7e18ad76807e38dd7d
derkalle4 <kalle@kandru.de>
2025-08-13 19:25:09 +0200

    bump version to 0.0.8

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace MiniAdmin
 {
     public partial class MiniAdmin
     {
-        public override string ModuleVersion => "0.0.7";
+        public override string ModuleVersion => "0.0.8";
     }
 }
